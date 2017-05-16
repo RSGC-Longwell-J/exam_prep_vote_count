9fcd51818bc21d25642769e895fcb59024b0c932
Longwell <Longwell@longwell.local>
2017-05-16 08:11:59 -0400

    Finished the code and it works

--- ExamTemplate/main.swift
+++ ExamTemplate/main.swift
@@ -1,74 +1,74 @@
 import Foundation
 
-/*
- 
- ORGANIZING YOUR SOLUTION
- 
- A good way to orgaize your code is to separate your code into the three sections - input, process, output – as much as possible.
- 
- The start of a solution is implemented below. Consider all the possible inputs. Can you finish the solution?
- 
- */
+//ORGANIZING YOUR SOLUTION
+//
+//     A good way to orgaize your code is to separate your code into the three sections - input, process, output – as much as possible.
+//
+// The start of a solution is implemented below. Consider all the possible inputs. Can you finish the solution?
 
-/*
- 
- INPUT
- 
- Be sure that your implementation of this section discards all the possible bad inputs the user could provide.
- 
- Make use of your test plan and algorithm to ensure your code is complete.
- 
- */
-var inputToProcess : String = ""
+var numberOfVoters : Int = 0
 
-// Loop until valid input is received
-while inputToProcess == "" {
+var LetterVotes : String = ""
+var output : String = ""
+
+var NumberOfAVotes : Int = 0
+var NumberOfBVote : Int = 0
+var VoteCount : Int = 0
+
+//Input
+while numberOfVoters == 0 {
     
-    // Show the prompt
-    print("Ask the question here? ", terminator: "")
-    
-    // Get the user's input
+    // Ask the user for input
+    print("How many voters? ", terminator: "")
     var input : String?
     input = readLine()
-    
-    // Use optional binding to see if the string can be unwrapped (to see if it is not nil)
     if let notNilInput = input {
-        
-        // You probably need to add additional checks to be sure the
-        // input received is valid
-        // Add checks as needed...
-        
-        // Save the input given, as we are certain it's what we are looking for now
-        inputToProcess = notNilInput
-        
+        if let inputAsInt = Int(notNilInput) {
+            if inputAsInt >= 1 || inputAsInt <= 15 {
+                numberOfVoters = inputAsInt
+            }
+        }
     }
+}
+//PROCESS
+//
+//     Here is where you implement the logic that solves the problem at hand.
+//
+// Make use of your test plan and algorithm to ensure your code is complete.
+while LetterVotes == "" {
+    var votesInput : String?
+    votesInput = readLine()
+    if let notNilInput = votesInput {
+        LetterVotes = notNilInput
+    }
+}
+
+
+//Logic to determin what the correct ouput will be depending on the input
+for letter in LetterVotes.characters {
+    if letter == "A" {
+        NumberOfAVotes += 1
+    } else if letter == "B" {
+        NumberOfBVote += 1
+    }
+    VoteCount += 1
+}
+if VoteCount != numberOfVoters {
+    
+    output = "ERROR"
+    
+} else if NumberOfAVotes == NumberOfBVote {
+    
+    output = "Tie"
+    
+} else if NumberOfAVotes > NumberOfBVote {
+    
+    output = "A"
+    
+} else if NumberOfBVote > NumberOfAVotes {
+    
+    output = "B"
     
 }
 
-/*
- 
- PROCESS
- 
- Here is where you implement the logic that solves the problem at hand.
- 
- Make use of your test plan and algorithm to ensure your code is complete.
- 
- */
-
-// Add 'process' code below....
-print("replace with process logic")
-
-
-/*
- 
- OUTPUT
- 
- Here is where you report the results of the 'process' section above.
- 
- Make use of your test plan and algorithm to ensure your code is complete.
- 
- */
-
-// Add 'output' code below... replace what is here as needed.
-print("The input given was: \(inputToProcess)")
-
+print(output)
